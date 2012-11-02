0c4662fe286d9eb77710eec92e1338c9c750be2d
oscarosorio88@gmail.com <oscarosorio88@gmail.com@b416f3bf-e919-ee76-9745-39849cef044f>
2012-11-02 05:08:22 +0000

    development login whit ajax part2

--- trunk/Contexto2/web/authentication/js/login.js
+++ trunk/Contexto2/web/authentication/js/login.js
@@ -20,18 +20,79 @@ $(document).ready(function() {
 	});
         
         
+        
+        $(".username").keydown(function(event) {
+            // Allow: backspace, delete, tab, escape, and enter
+            if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
+                 // Allow: Ctrl+A
+                (event.keyCode == 65 && event.ctrlKey === true) || 
+                 // Allow: home, end, left, right
+                (event.keyCode >= 35 && event.keyCode <= 39)) {
+                     // let it happen, don't do anything
+                     return;
+            }
+            else {
+                // Ensure that it is a number and stop the keypress
+                if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                    event.preventDefault(); 
+                }   
+            }
+        });
+        
+        
+      
+        $( "#dialog" ).dialog({
+            autoOpen: false,
+            show: "blind",
+            hide: "explode",
+            width: 305,
+            height: 480,
+            modal:true,
+            buttons: {
+                Ok: function() {
+                    $( this ).dialog( "close" );
+                }
+            }
+        });
+        
+        
         $("#login").click(function(){
+            
+         var user=   $('input[name="username"]').val();
+         var pass=   $('input[name="password"]').val();
+         
+         var json=  {
+           "ac": 0,
+           "op": 0,
+           "data": {
+             "cedula": user,
+             "password": pass
+           }
+      }
              $.ajax({
                               type: "POST",
                               url: "../../Facade",
-                              data: $(".login-form").val()
-                           
+                              data: JSON.stringify(json),
+                              contentType: "application/json; charset=utf-8",
+                              dataType: "json"
                               
                             }).done(function( msg ) {
-                             // alert( "Data Saved: " + msg );
-                              //console.info(msg);
+                              
                               try{
                               console.info(msg);
+                              
+                              if(msg.login=="false")
+                                  {
+                                   $(".contentM").html(msg.cause);
+                                   $( "#dialog" ).dialog("open");   
+                                  }
+                              else
+                                  {
+                                   $(".contentM").html("Welcome "+msg.userInfo.name);
+                                   $( "#dialog" ).dialog("open");
+                                  }
+                              
+                              
                               }catch(e)
                               {
                                console.info(msg);    
